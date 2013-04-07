8eb9bd91ce524f19f0a92a3b8e9a59e1d06b98ca
liuyazheng <liuyazheng82@gmail.com>
2013-04-07 17:21:58 +0800

    copy net-http-cheat-sheet code

---
+++ net-http-cheat-sheet-master/post_form.rb
@@ -0,0 +1,18 @@
+require "net/http"
+require "uri"
+
+uri = URI.parse("http://example.com/search")
+
+# Shortcut
+response = Net::HTTP.post_form(uri, {"q" => "My query", "per_page" => "50"})
+
+# Full control
+http = Net::HTTP.new(uri.host, uri.port)
+
+request = Net::HTTP::Post.new(uri.request_uri)
+request.set_form_data({"q" => "My query", "per_page" => "50"})
+
+# Tweak headers, removing this will default to application/x-www-form-urlencoded 
+request["Content-Type"] = "application/json"
+
+response = http.request(request)
